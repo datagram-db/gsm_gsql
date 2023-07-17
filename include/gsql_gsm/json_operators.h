b792cc5214f09eb67c72d0098448a4957cf11289
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-07-17 22:30:16 +0100

    Good copyright

--- include/gsql_gsm/json_operators.h
+++ include/gsql_gsm/json_operators.h
@@ -1,3 +1,23 @@
+/*
+ * main.cpp
+ * This file is part of gsm_gsql
+ *
+ * Copyright (C) 2023 - Wiktor Zegadło
+ *
+ * gsm_gsql is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * gsm_gsql is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gsm_gsql. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //
 // Created by neo on 23/04/23.
 //
